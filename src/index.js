ece0e6df261717e94ad3cfe868f8bb2927989b81
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-02 23:43:14 -0700

    Refactoring a Class to a Function

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner'
 
-class App extends React.Component {
+const App = () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    state = { lat: null, errorMessage: '' }
-
-    // Lifecycle method.
-    componentDidMount() {
+    useEffect(()=> {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
-        );
+            position => setLat(position.coords.latitude), 
+            err => setErrorMessage(err.message)
+        )
+    }, []);
+
+    let content;
+    if (errorMessage){
+        content = <div>Error: {errorMessage}</div>
+    } else if (lat){
+        content = <SeasonDisplay lat={lat}/>
+    } else {
+        content = <Spinner message="Please accept location request"/>
     }
 
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Erro: {this.state.errorMessage}</div>
-        }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        return <Spinner />
-    }
-
-    render() {
-        return <div className="red border">{this.renderContent()}</div>
-        
-    }
-}
+    return <div className="border red">{content}</div>
+};
 
 ReactDOM.render(
     <App />,
